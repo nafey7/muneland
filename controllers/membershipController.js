23c57290002763e0f052457a0e840c3624ea7767
nafey7 <nafeymoiz07@gmail.com>
2023-03-31 12:37:42 +0500

    email code inserted in webhook

--- controllers/membershipController.js
+++ controllers/membershipController.js
@@ -161,11 +161,41 @@ exports.StripeWebhook =  async (req, res) => {
         const queryClient = Client.findOne({_id: event.data.object.metadata.clientID});
         const clientInfo = await queryClient;
 
-        console.log('This is the plan', clientInfo.plan);
-        
-        if (MembershipName.plan == []){
-            let emailSend = await helperController.SendEmail(clientInfo);
-        }
+        let transporter = nodemailer.createTransport({
+            host: "smtp-mail.outlook.com",
+            port: 587,
+            secure: false,
+            auth: {
+              user: process.env.EMAIL,
+              pass: process.env.PASSWORD
+            },
+            tls: {
+                ciphers:'SSLv3'
+            }
+          });
+          transporter.verify(function (error, success) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log("Server is ready to take our messages");
+            }
+          });
+          let mailOptions = {
+            from: process.env.EMAIL,
+            to: clientInfo.emailAddress,
+            subject: 'Welcome to Muneland',
+            html: `<p>Use the following PIN for authentication</p>`
+          };
+          
+          await transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+                throw new Error ('Unexpected Error while sending Email')
+            } else {
+                console.log('Email sent: ' + info.response);
+                
+            }
+          });
 
         const filter = {_id: event.data.object.metadata.clientID};
         // const update = {plan: event.data.object.metadata.membershipID};
